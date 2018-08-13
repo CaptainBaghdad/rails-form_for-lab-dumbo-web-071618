35e477cc161b106acbbe812ac93461d765bf2f6f
Jason Pitts <juniorjr083@gmail.com>
2018-08-13 18:23:06 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,24 @@
 class StudentsController < ApplicationController
+  
+  def index 
+     @student = Student.all 
+       
+  end 
+  
+  def new 
+    
+  end 
+  
+  def show 
+    @student = Student.find(params[:id])
+    
+    
+  end 
+  
+  def create 
+    @student = Student.create()
+    
+  end 
+  
 end
+
