52dd4b72a84efd309977e89bb3fb9038c81793ad
Jason Pitts <juniorjr083@gmail.com>
2018-08-13 18:48:29 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,13 +9,13 @@ class SchoolClassesController < ApplicationController
   end 
   
   def show 
-    @student = Student.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
     
     
   end 
   
   def create 
-    @student = Student.new(student_params(:first_name, :last_name))
+    @school_class = SchoolClass.create(student_params(:first_name, :last_name))
     @student.save
 
     redirect_to student_path(@student)
